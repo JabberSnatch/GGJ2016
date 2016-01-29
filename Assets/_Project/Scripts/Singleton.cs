182331873d53d9a0494c3ea8f661b748ddeb80aa
Tom Georgin <tom.georgin@gmail.com>
2016-01-30 00:44:18 +0100

    Singleton commit

---
+++ Assets/_Project/Scripts/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static bool onApplicationQuit = false;
+
+	private static T instance;
+	public static T Instance
+	{
+		get
+		{
+			if (onApplicationQuit)
+			{
+				return null;
+			}
+
+			if (null == instance)
+			{
+
+				instance = GameObject.FindObjectOfType<T>();
+				T[] instances = GameObject.FindObjectsOfType<T>();
+
+				if (instances.Length > 1)
+				{
+					Debug.LogError("Something got wrong, because you have several singleton for the following type: " + typeof(T));
+					return instance;
+				}
+			}
+
+			return instance;
+		}
+	}
+
+	private void onDestroy()
+	{
+		onApplicationQuit = true;
+	}
+}
