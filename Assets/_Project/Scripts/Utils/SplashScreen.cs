2c3a0a25f77d5d80475d7ba7eb2df0ab894a574e
Samuel Bourasseau <samuel.bourasseau@gmail.com>
2016-01-31 14:11:29 +0100

    Adds splash screen

---
+++ Assets/_Project/Scripts/Utils/SplashScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour
+{
+    public void Awake()
+    {
+        StartCoroutine(waitCoroutine());
+    }
+
+    public IEnumerator waitCoroutine()
+    {
+        yield return new WaitForSeconds(8f);
+        Application.LoadLevel("prod_level");
+    }
+}
